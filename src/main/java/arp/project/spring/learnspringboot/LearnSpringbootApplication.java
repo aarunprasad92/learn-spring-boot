1d850fae5d7884cbeb60c777684c4bbffaf0067d
Arun Prasad <aarunprasad92@gmail.com>
2023-02-15 23:07:44 +0530

    first commit

---
+++ src/main/java/arp/project/spring/learnspringboot/LearnSpringbootApplication.java
@@ -0,0 +1,13 @@
+package arp.project.spring.learnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringbootApplication.class, args);
+	}
+
+}
