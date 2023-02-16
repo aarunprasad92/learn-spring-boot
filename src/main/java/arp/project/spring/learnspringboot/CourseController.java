7f11230d58dc9091696a1267284238c9f68516c5
Arun Prasad <aarunprasad92@gmail.com>
2023-02-16 22:40:39 +0530

    Create a rest api

---
+++ src/main/java/arp/project/spring/learnspringboot/CourseController.java
@@ -0,0 +1,16 @@
+package arp.project.spring.learnspringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(new Course(1, "Learn AWS", "Tom"),
+                new Course(2, "Learn DevOps", "Harry"));
+    }
+}
