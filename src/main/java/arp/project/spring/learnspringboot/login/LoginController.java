283d5f343a9e03d456051eb2b0ed6e8d33717ab7
Arun Prasad <aarunprasad92@gmail.com>
2023-02-17 21:33:34 +0530

    Add logging

--- src/main/java/arp/project/spring/learnspringboot/login/LoginController.java
+++ src/main/java/arp/project/spring/learnspringboot/login/LoginController.java
@@ -1,5 +1,7 @@
 package arp.project.spring.learnspringboot.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
     @RequestMapping("login")
     public String login(@RequestParam String name, ModelMap model) {
         model.put("name", name);
+        logger.debug("Request param is {}", name);
         return "login";
     }
 }
