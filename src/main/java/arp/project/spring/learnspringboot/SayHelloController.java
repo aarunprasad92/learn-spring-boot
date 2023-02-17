13c6c2e9bae710d4d3a735397a3364f599820b33
Arun Prasad <aarunprasad92@gmail.com>
2023-02-17 20:55:54 +0530

    Add a controller

---
+++ src/main/java/arp/project/spring/learnspringboot/SayHelloController.java
@@ -0,0 +1,14 @@
+package arp.project.spring.learnspringboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello() {
+        return "hello world";
+    }
+}
