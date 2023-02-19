ab1aeaa205b20b43c2c8e7c7878e37adc99f16a3
Arun Prasad <aarunprasad92@gmail.com>
2023-02-20 00:28:43 +0530

    Add todo service

---
+++ src/main/java/arp/project/spring/learnspringboot/todo/TodoService.java
@@ -0,0 +1,19 @@
+package arp.project.spring.learnspringboot.todo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoService {
+    private static List<Todo> todoList = new ArrayList<>();
+
+    static {
+        todoList.add(new Todo(1, "arun", "Learn AWS", LocalDate.now().plusMonths(3), false));
+        todoList.add(new Todo(2, "arun", "Learn Docker", LocalDate.now().plusMonths(4), false));
+        todoList.add(new Todo(3, "arun", "Learn Postgres", LocalDate.now().plusMonths(5), false));
+    }
+
+    public List<Todo> findByUsername(String username) {
+        return todoList;
+    }
+}
