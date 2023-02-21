744ab03d86c7c1e0cf1ef04e9fb21c6d36b93cc4
Arun Prasad <aarunprasad92@gmail.com>
2023-02-21 23:22:11 +0530

    Insert more rows

--- src/main/java/arp/project/spring/learnspringboot/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/arp/project/spring/learnspringboot/jdbc/CourseJdbcCommandLineRunner.java
@@ -12,5 +12,10 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         repository.insert(new Course(1, "Learn AWS Lambda", "arun"));
+        repository.insert(new Course(2, "Learn Docker", "arun"));
+        repository.insert(new Course(3, "Learn Kubernetes", "arun"));
+        repository.insert(new Course(4, "Learn Spring boot", "arun"));
+        repository.insert(new Course(5, "Learn Hibernate", "arun"));
+
     }
 }
