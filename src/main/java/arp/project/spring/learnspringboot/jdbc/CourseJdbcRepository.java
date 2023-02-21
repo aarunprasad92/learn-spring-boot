fce855e4477ddf4779baea3d6b36d850fc0d79ec
Arun Prasad <aarunprasad92@gmail.com>
2023-02-21 23:13:24 +0530

    Insert row in db using spring jdbc

---
+++ src/main/java/arp/project/spring/learnspringboot/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,21 @@
+package arp.project.spring.learnspringboot.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+    @Autowired
+    private JdbcTemplate springJdbcTemplate;
+    private static String INSERT_QUERY =
+            """
+            insert into course (id, name, author) 
+            values(1, 'Learn AWS', 'arun');
+            """;
+    public void insert() {
+        springJdbcTemplate.update(INSERT_QUERY);
+    }
+
+
+}
