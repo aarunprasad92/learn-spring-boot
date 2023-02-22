4d9d87d5fb6b9f9130d7f1e1d6a44148f78c08df
Arun Prasad <aarunprasad92@gmail.com>
2023-02-22 13:01:46 +0530

    Add more rows

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,14 @@
 insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
 values (10001, 'arun', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10002, 'arun', 'Get Azure Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10003, 'arun', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10004, 'arun', 'Get DevOps Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10005, 'arun', 'Get Security Certified', CURRENT_DATE(), false);
