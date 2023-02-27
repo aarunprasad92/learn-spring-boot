86dfff905b5b6b15f0be4e40408902d6a2e2ae96
Arun Prasad <aarunprasad92@gmail.com>
2023-02-27 23:18:11 +0530

    rest api - Add path param in url

--- src/main/java/arp/project/spring/learnspringboot/dummyapis/DummyController.java
+++ src/main/java/arp/project/spring/learnspringboot/dummyapis/DummyController.java
@@ -1,5 +1,6 @@
 package arp.project.spring.learnspringboot.dummyapis;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,15 @@ public class DummyController {
     @RequestMapping("/hello-bean")
     public Hello helloBean() {
         return new Hello("Hello world");
+    }
+
+    @RequestMapping("/hello-path-param/{name}")
+    public Hello helloBeanPathParam(@PathVariable String name) {
+        return new Hello("Hello world, " + name);
+    }
+
+    @RequestMapping("/hello-path-param/{name}/message/{message}")
+    public Hello helloBeanMultiplePathParam(@PathVariable String name, @PathVariable String message) {
+        return new Hello(message + ", " + name);
     }
 }
