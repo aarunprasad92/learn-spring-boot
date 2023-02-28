4a8a77166f6dc47d8dc5eb263c48f287ba5e85d8
Arun Prasad <aarunprasad92@gmail.com>
2023-03-01 00:17:03 +0530

    integration test - assert status code and content type

--- src/test/java/arp/project/spring/learnspringboot/survey/SurveyControllerIntegrationTest.java
+++ src/test/java/arp/project/spring/learnspringboot/survey/SurveyControllerIntegrationTest.java
@@ -6,13 +6,20 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SurveyControllerIntegrationTest {
-    // http://localhost:8081/surveys/Survey1/questions/Question1
+    private static String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
+
+    // used to fire the api request
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     // text blocks very useful since java 15
     String str = """
             {
@@ -28,10 +35,6 @@ public class SurveyControllerIntegrationTest {
             }
             """;
 
-    private static String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
-    // used to fire the api request
-    @Autowired
-    private TestRestTemplate restTemplate;
 
     @Test
     void getQuestionByIdForSurvey_baseScenario() throws JSONException {
@@ -43,6 +46,13 @@ public class SurveyControllerIntegrationTest {
                 "description":"Most Popular Cloud Platform Today",
                 "correctAnswer":"AWS"}
                 """;
+        //check status code is 200
+        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+
+        //check content type is application json
+        assertEquals("application/json", responseEntity.getHeaders().getContentType().toString());
+
+        //check actual response body
         JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
     }
 }
