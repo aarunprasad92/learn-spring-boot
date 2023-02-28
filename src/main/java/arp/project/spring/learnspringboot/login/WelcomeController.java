1175dc731dffdfdb041c27bbc69d78c7403acc2d
Arun Prasad <aarunprasad92@gmail.com>
2023-02-28 23:48:51 +0530

    integration test - add api test and disabled spring security

--- src/main/java/arp/project/spring/learnspringboot/login/WelcomeController.java
+++ src/main/java/arp/project/spring/learnspringboot/login/WelcomeController.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 //import org.springframework.security.core.Authentication;
 //import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +21,8 @@ public class WelcomeController {
     }
 
     private String getLoggedInUserName() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return authentication.getName();
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        return authentication.getName();
+        return "arun";
     }
 }
---
+++ src/test/java/arp/project/spring/learnspringboot/survey/SurveyControllerIntegrationTest.java
@@ -0,0 +1,42 @@
+package arp.project.spring.learnspringboot.survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SurveyControllerIntegrationTest {
+    // http://localhost:8081/surveys/Survey1/questions/Question1
+    // text blocks very useful since java 15
+    String str = """
+            {
+            "id": "Question1",
+            "description": "Most Popular Cloud Platform Today",
+            "options": [
+            "AWS",
+            "Azure",
+            "Google Cloud",
+            "Oracle Cloud"
+            ],
+            "correctAnswer": "AWS"
+            }
+            """;
+
+    private static String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
+    // used to fire the api request
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void getQuestionByIdForSurvey_baseScenario() {
+        ResponseEntity<String> responseEntity = restTemplate
+                .withBasicAuth("arun", "dummy")
+                .getForEntity(SPECIFIC_QUESTION_URL, String.class);
+        System.out.println(responseEntity.getBody());
+        System.out.println(responseEntity.getHeaders());
+    }
+}
