2efa34f7198c42c69ab12e95b078801d4cd0460c
Arun Prasad <aarunprasad92@gmail.com>
2023-03-01 19:26:57 +0530

    Functional Programming - Print list using a filter

--- src/main/java/arp/project/learnfunctionalprogramming/FP01Functional.java
+++ src/main/java/arp/project/learnfunctionalprogramming/FP01Functional.java
@@ -5,11 +5,20 @@ import java.util.List;
 public class FP01Functional {
     public static void main(String[] args) {
         System.out.println("Hello");
-        printAllNumbersInListFunctional(List.of(12, 9, 3, 66, 12));
+        //printAllNumbersInListFunctional(List.of(12, 9, 3, 66, 12));
+        printEvenNumbersInListFunctional(List.of(12, 9, 3, 66, 12));
     }
 
     private static void printAllNumbersInListFunctional(List<Integer> numbers) {
         // convert to stream, method reference for each number
         numbers.stream().forEach(System.out::println);
+    }
+
+    private static boolean isEven(int number) {
+        return number %2 == 0;
+    }
+
+    private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream().filter(FP01Functional::isEven).forEach(System.out::println);
     }
 }
--- src/main/java/arp/project/learnfunctionalprogramming/FP01Structured.java
+++ src/main/java/arp/project/learnfunctionalprogramming/FP01Structured.java
@@ -5,12 +5,20 @@ import java.util.List;
 public class FP01Structured {
     public static void main(String[] args) {
         System.out.println("Hello");
-        printAllNumbersInListStructured(List.of(12, 9, 3, 66, 12));
+       // printAllNumbersInListStructured(List.of(12, 9, 3, 66, 12));
+        printEvenNumbersInListStructured(List.of(12, 9, 3, 66, 12));
     }
 
     private static void printAllNumbersInListStructured(List<Integer> numbers) {
         for(int number: numbers) {
             System.out.println(number);
+        }
+    }
+
+    private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+        for(int number: numbers) {
+            if (number % 2 == 0)
+                System.out.println(number);
         }
     }
 }
