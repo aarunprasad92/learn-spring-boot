f1a0863ff59978aae7c9a3e11966dca3a40f72bf
Arun Prasad <aarunprasad92@gmail.com>
2023-03-01 19:10:07 +0530

    Functional Programming - Print list in traditional way

---
+++ src/main/java/arp/project/learnfunctionalprogramming/FP01Structured.java
@@ -0,0 +1,16 @@
+package arp.project.learnfunctionalprogramming;
+
+import java.util.List;
+
+public class FP01Structured {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        printAllNumbersInListStructured(List.of(12, 9, 3, 66, 12));
+    }
+
+    private static void printAllNumbersInListStructured(List<Integer> numbers) {
+        for(int number: numbers) {
+            System.out.println(number);
+        }
+    }
+}
