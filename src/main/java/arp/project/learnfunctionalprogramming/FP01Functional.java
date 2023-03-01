ae7705b3c2e3f3828d06836c8316fe1ad206a50c
Arun Prasad <aarunprasad92@gmail.com>
2023-03-01 19:16:32 +0530

    Functional Programming - Print list using stream and method reference

---
+++ src/main/java/arp/project/learnfunctionalprogramming/FP01Functional.java
@@ -0,0 +1,19 @@
+package arp.project.learnfunctionalprogramming;
+
+import java.util.List;
+
+public class FP01Functional {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        printAllNumbersInListFunctional(List.of(12, 9, 3, 66, 12));
+    }
+
+    private static void print(int number) {
+        System.out.println(number);
+    }
+
+    private static void printAllNumbersInListFunctional(List<Integer> numbers) {
+        // convert to stream, method reference for each number
+        numbers.stream().forEach(FP01Functional::print);
+    }
+}
