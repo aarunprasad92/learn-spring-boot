bf0f2976b407e4d4187d52e8340c82efbf1552c7
Arun Prasad <aarunprasad92@gmail.com>
2023-03-01 19:35:48 +0530

    Functional Programming - Using map function to transform the filtered values

--- src/main/java/arp/project/learnfunctionalprogramming/FP01Functional.java
+++ src/main/java/arp/project/learnfunctionalprogramming/FP01Functional.java
@@ -6,7 +6,7 @@ public class FP01Functional {
     public static void main(String[] args) {
         System.out.println("Hello");
         //printAllNumbersInListFunctional(List.of(12, 9, 3, 66, 12));
-        printEvenNumbersInListFunctional(List.of(12, 9, 3, 66, 12));
+        printSquaresOfEvenNumbersInListFunctional(List.of(12, 9, 3, 66, 12));
     }
 
     private static void printAllNumbersInListFunctional(List<Integer> numbers) {
@@ -21,6 +21,13 @@ public class FP01Functional {
     private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
         numbers.stream()
                 .filter(number -> number%2 == 0)
+                .forEach(System.out::println);
+    }
+
+    private static void printSquaresOfEvenNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream()
+                .filter(number -> number%2 == 0)
+                .map(number -> number * number)
                 .forEach(System.out::println);
     }
 }
